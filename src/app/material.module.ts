6d3931f554a8095fd0b2be09c654349a86f1c03a
erbpf <erik.butcher@payfactors.com>
2018-06-20 08:47:35 -0400

    initial

---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+import { MatButtonModule, MatInputModule } from '@angular/material'
+import { MatIconModule } from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+    imports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ],
+    exports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ]
+})
+export class MaterialModule{};
