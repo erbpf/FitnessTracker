cca5fa1c981de1d94fa8203147f2418026584f1c
erbpf <erik.butcher@payfactors.com>
2018-06-20 10:56:18 -0400

    completed signup validation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core'
-import { MatButtonModule, MatInputModule } from '@angular/material'
+import { MatButtonModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material'
 import { MatIconModule } from '@angular/material/icon'
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-    imports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ],
-    exports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ]
+    imports: [ 
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule
+    ],
+    exports: [ 
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule
+    ]
 })
 export class MaterialModule{};
