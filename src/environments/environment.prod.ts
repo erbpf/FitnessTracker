b6355994cfa03dfdd704bad25f4c3860a30b2b9b
erbpf <erik.butcher@payfactors.com>
2018-06-21 17:14:03 -0400

    built production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCu4vW_M0UyPm6xaH1rUiTaScjKVmdMK6s",
+    authDomain: "fitnesstracker-f2316.firebaseapp.com",
+    databaseURL: "https://fitnesstracker-f2316.firebaseio.com",
+    projectId: "fitnesstracker-f2316",
+    storageBucket: "fitnesstracker-f2316.appspot.com",
+    messagingSenderId: "660303150540"
+  }
 };
