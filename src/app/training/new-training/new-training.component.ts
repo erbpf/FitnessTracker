b9f6f738b161448c03f378ce7c521a0ff16a63c9
erbpf <erik.butcher@payfactors.com>
2018-06-21 01:53:18 -0400

    introduced firestorm with rxjs mapping

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from "rxjs";
+// import { map } from "rxjs/operators";
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-new-training',
@@ -9,13 +13,26 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[] = [];
+  exercises: Observable<any>;
   selectedExerciseId: string;
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+    // this.exercises = this.db.collection('availableExercises').valueChanges();
+    //  this.db.collection('availableExercises').snapshotChanges().subscribe(result => {for (const res of result) { console.log(res)});
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            ...doc.payload.doc.data()
+          };
+        });
+      });
+   
   }
 
   onStartTraining(form: NgForm) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCu4vW_M0UyPm6xaH1rUiTaScjKVmdMK6s",
+    authDomain: "fitnesstracker-f2316.firebaseapp.com",
+    databaseURL: "https://fitnesstracker-f2316.firebaseio.com",
+    projectId: "fitnesstracker-f2316",
+    storageBucket: "fitnesstracker-f2316.appspot.com",
+    messagingSenderId: "660303150540"
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
